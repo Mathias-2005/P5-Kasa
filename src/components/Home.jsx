d22cb8f0d39cc44f5d366feea1f06969e1786404
Mathias <mathiasmaillydepinho@gmail.com>
2026-01-02 14:50:31 +0100

    add background headline

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
+import Slogan from "./Slogan";
+
 function Home () {
 
     return <div>
         <p>TEST 1 PAGE 1</p>
+        <Slogan />
         </div>
 }
 
---
+++ src/components/Slogan.jsx
@@ -0,0 +1,12 @@
+import "./Slogan.scss"
+
+function Slogan () {
+
+    return <div>
+        <div className="background__img">
+            <div className="background__img--content">Chez vous, partout et ailleurs</div>
+        </div>
+    </div>
+}
+
+export default Slogan;
